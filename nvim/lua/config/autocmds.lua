f17b3b1f74500d8a5eb6a17db3b2c30cb0a9be20
MaoX_Yu <maoxytz@163.com>
2023-12-26 21:52:51 +0800

    feat: use tab in golang

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Use <tab> in golang
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "go", "gomod", "gosum", "gowork" },
+  callback = function()
+    vim.opt.expandtab = false
+  end,
+})
