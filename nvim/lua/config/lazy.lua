f8abb0d5e1eb373d8ae0ec1968b2d01e1e3098d2
MaoX_Yu <maoxytz@163.com>
2023-12-27 20:50:04 +0800

    style: format code

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,16 +1,9 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
+  print("Downloading lazy...")
   -- stylua: ignore
-  print "Downloading lazy..."
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
