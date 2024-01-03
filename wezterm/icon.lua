9d3b5bbfe7a193bd6da4f9a21f38f7458391bbe5
MaoX_Yu <maoxytz@163.com>
2024-01-03 22:34:05 +0800

    feat: update tab bar

--- wezterm/icon.lua
+++ wezterm/icon.lua
@@ -2,8 +2,10 @@ local Wezterm = require("wezterm")
 
 local M = {}
 
-M.SOLID_LEFT_TRIANGLE = Wezterm.nerdfonts.ple_upper_left_triangle
-M.SOLID_RIGHT_TRIANGLE = Wezterm.nerdfonts.ple_lower_right_triangle
+-- M.SOLID_LEFT_TRIANGLE = Wezterm.nerdfonts.ple_upper_left_triangle
+-- M.SOLID_RIGHT_TRIANGLE = Wezterm.nerdfonts.ple_lower_right_triangle
+M.SOLID_LEFT_TRIANGLE = Wezterm.nerdfonts.ple_lower_left_triangle
+M.SOLID_RIGHT_TRIANGLE = Wezterm.nerdfonts.ple_upper_right_triangle
 
 M.nerdfonts = {
   cargo = "󱘗",
