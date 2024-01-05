c6518585ce51947730ede203cd3dccd72ca45ed4
MaoX_Yu <maoxytz@163.com>
2024-01-05 22:53:48 +0800

    feat: update fonts & colors

--- wezterm/terminal.lua
+++ wezterm/terminal.lua
@@ -10,6 +10,8 @@ M.colors = {
   tab_active = "#FAB387",
   tab_inactive = "#7AA2F7",
   tab_fg = "#1E1E2E",
+  float_bg = "#303446",
+  float_fg = "rgba(198, 208, 245, 1)",
 }
 
 function M.setup(config)
@@ -27,7 +29,7 @@ function M.setup(config)
       family = "Maple Mono SC NF",
       harfbuzz_features = { "ss01=1", "ss02=1", "ss03=1", "ss04=1", "ss05=1" },
     },
-    "Noto Color Emoji",
+    "Segoe UI Symbol",
   })
   config.font_size = 14
 
@@ -57,7 +59,6 @@ function M.setup(config)
   config.enable_scroll_bar = false
   config.inactive_pane_hsb = { saturation = 1.0, brightness = 1.0 }
   config.window_padding = { left = "5px", right = "5px", top = "0.1cell", bottom = "0.1cell" }
-  config.window_close_confirmation = "NeverPrompt"
 
   -- Tab bar
   config.colors = {
@@ -102,6 +103,15 @@ function M.setup(config)
   config.tab_bar_at_bottom = true
   config.show_new_tab_button_in_tab_bar = true
   config.use_fancy_tab_bar = false
+
+  -- Command palette
+  config.command_palette_bg_color = M.colors.float_bg
+  config.command_palette_fg_color = M.colors.float_fg
+  -- config.command_palette_rows = 25 -- only nightly
+
+  -- Char select
+  config.char_select_bg_color = M.colors.float_bg
+  config.char_select_fg_color = M.colors.float_fg
 
   -- Other
   config.adjust_window_size_when_changing_font_size = true
