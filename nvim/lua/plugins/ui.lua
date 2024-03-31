50397cc3eb6f3941c5cfe64d5eda55773f35d4be
MaoX_Yu <maoxytz@163.com>
2024-03-31 22:14:07 +0800

    refactor(catppuccin): move highlights config

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -26,58 +26,7 @@ return {
         types = { "italic" },
       },
       custom_highlights = function(C)
-        return {
-          PmenuSbar = { bg = C.mantle },
-          Visual = { bg = C.surface1, reverse = true, style = { "bold" } },
-          VisualNOS = { bg = C.surface1, reverse = true, style = { "bold" } },
-
-          -- lazy.nvim
-          LazyButtonActive = { bg = C.surface1, style = { "bold" } },
-
-          -- lspsaga
-          SagaBorder = { fg = C.mantle, bg = C.mantle },
-          SagaNormal = { bg = C.mantle },
-          SagaBeacon = { bg = C.mantle },
-
-          -- which-key
-          WhichKeyBorder = { fg = C.blue, bg = C.mantle },
-
-          -- scrollview
-          ScrollView = { bg = C.surface1 },
-
-          -- nvim-cmp
-          CmpItemAbbr = { fg = C.text },
-          CmpItemMenu = { fg = C.mauve },
-          CmpItemSel = { fg = C.mantle, bg = C.green, style = { "bold" } },
-          CmpItemAbbrMatch = { fg = C.blue, style = { "bold" } },
-          CmpItemAbbrMatchFuzzy = { fg = C.blue, style = { "bold" } },
-          CmpItemKindSnippet = { fg = C.crust, bg = C.mauve },
-          CmpItemKindKeyword = { fg = C.crust, bg = C.red },
-          CmpItemKindText = { fg = C.crust, bg = C.teal },
-          CmpItemKindMethod = { fg = C.crust, bg = C.blue },
-          CmpItemKindConstructor = { fg = C.crust, bg = C.blue },
-          CmpItemKindFunction = { fg = C.crust, bg = C.blue },
-          CmpItemKindFolder = { fg = C.crust, bg = C.blue },
-          CmpItemKindModule = { fg = C.crust, bg = C.blue },
-          CmpItemKindConstant = { fg = C.crust, bg = C.peach },
-          CmpItemKindField = { fg = C.crust, bg = C.green },
-          CmpItemKindProperty = { fg = C.crust, bg = C.green },
-          CmpItemKindEnum = { fg = C.crust, bg = C.green },
-          CmpItemKindUnit = { fg = C.crust, bg = C.green },
-          CmpItemKindClass = { fg = C.crust, bg = C.yellow },
-          CmpItemKindVariable = { fg = C.crust, bg = C.flamingo },
-          CmpItemKindFile = { fg = C.crust, bg = C.blue },
-          CmpItemKindInterface = { fg = C.crust, bg = C.yellow },
-          CmpItemKindColor = { fg = C.crust, bg = C.red },
-          CmpItemKindReference = { fg = C.crust, bg = C.red },
-          CmpItemKindEnumMember = { fg = C.crust, bg = C.red },
-          CmpItemKindStruct = { fg = C.crust, bg = C.blue },
-          CmpItemKindValue = { fg = C.crust, bg = C.peach },
-          CmpItemKindEvent = { fg = C.crust, bg = C.blue },
-          CmpItemKindOperator = { fg = C.crust, bg = C.blue },
-          CmpItemKindTypeParameter = { fg = C.crust, bg = C.blue },
-          CmpItemKindCopilot = { fg = C.crust, bg = C.teal },
-        }
+        return require("plugins.configs.catppuccin").highlights(C)
       end,
     },
   },
