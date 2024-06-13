7bc04aed26080de9a8b4dce625890c5708be40fb
MaoX_Yu <maoxytz@163.com>
2024-06-13 21:19:53 +0800

    feat: remove actions-preview

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,7 +4,6 @@ return {
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "K", false }
-      keys[#keys + 1] = { "<leader>ca", false }
     end,
     opts = {
       format = {
