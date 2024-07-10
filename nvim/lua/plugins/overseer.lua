240e0617b78cfe69ac1366774dfdc08c9b9f250b
MaoX_Yu <maoxytz@163.com>
2024-07-10 23:53:12 +0800

    fix(overseer): which_key desc don't show

--- nvim/lua/plugins/overseer.lua
+++ nvim/lua/plugins/overseer.lua
@@ -3,7 +3,7 @@ return {
     "stevearc/overseer.nvim",
     optional = true,
     keys = {
-      { "<leader>o", "", desc = " Overseer", mode = { "n", "v" } },
+      { "<leader>o", "", desc = " Overseer", mode = { "n", "v" } },
     },
   },
 }
