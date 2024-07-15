1f18b14870ea32bb4b3cd1e623ccf7bbd83f69cd
MaoX_Yu <maoxytz@163.com>
2024-07-15 23:09:25 +0800

    feat: update which_key

--- nvim/lua/plugins/which_key.lua
+++ nvim/lua/plugins/which_key.lua
@@ -7,17 +7,30 @@ return {
       win = {
         height = { min = 4, max = 999 },
       },
+      expand = function(node)
+        return not node.desc -- expand all nodes without a description
+      end,
       icons = {
         group = "",
         rules = {
           { plugin = "dropbar.nvim", icon = " ", color = "green" },
           { plugin = "overseer.nvim", icon = " ", color = "yellow" },
+          { plugin = "undotree", icon = " ", color = "green" },
+        },
+        keys = {
+          BS = "󰁮 ",
         },
       },
       spec = {
         { "=", group = "apply filter" },
         { "<", group = "indent left" },
         { ">", group = "indent right" },
+        { "f", desc = "Find Next Char" },
+        { "F", desc = "Find Prev Char" },
+        { "t", desc = "Find Next Char Before" },
+        { "T", desc = "Find Prev Char Before" },
+        { ";", desc = "Next Find Result" },
+        { ",", desc = "Prev Find Result" },
         { "<leader>", group = "leader" },
         { "<leader>S", group = "session" },
         { "<leader>o", group = "overseer", icon = { icon = " ", color = "yellow" } },
