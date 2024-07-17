e71549f1c7effc64ef4b8d9fc4d8110f3dd92057
MaoX_Yu <maoxytz@163.com>
2024-07-17 23:06:32 +0800

    fix(fittencode): lazy load and cleanup

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -5,19 +5,19 @@ end
 return {
   {
     "luozhiya/fittencode.nvim",
+    event = "InsertEnter",
     opts = {
       completion_mode = "source",
     },
-    dependencies = {
-      {
-        "hrsh7th/nvim-cmp",
-        ---@param opts cmp.ConfigSchema
-        opts = function(_, opts)
-          local cmp = require("cmp")
-          opts.sources = cmp.config.sources(vim.list_extend({ { name = "fittencode", group_index = 1 } }, opts.sources))
-          return opts
-        end,
-      },
-    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend({ { name = "fittencode", group_index = 1 } }, opts.sources))
+      return opts
+    end,
   },
 }
