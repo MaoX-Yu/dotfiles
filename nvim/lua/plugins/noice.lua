387bbbe56401572cd8271cbfe234d899ebebe001
MaoX_Yu <maoxytz@163.com>
2024-08-29 23:14:00 +0800

    feat(nvim): cmdline style

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/noice.nvim",
+    optional = true,
+    opts = {
+      presets = {
+        command_palette = false,
+      },
+      cmdline = {
+        view = "cmdline",
+      },
+    },
+  },
+}
