768c8e9097686721d1540b59284a8a8e09ca132a
MaoX_Yu <maoxytz@163.com>
2024-09-09 19:47:37 +0800

    feat(nvim): add fidget

--- lazyvim/lua/plugins/noice.lua
+++ lazyvim/lua/plugins/noice.lua
@@ -1,17 +1,6 @@
 return {
   {
     "folke/noice.nvim",
-    optional = true,
-    opts = {
-      cmdline = {
-        enabled = false,
-      },
-      messages = {
-        enabled = false,
-      },
-      popupmenu = {
-        enabled = false,
-      },
-    },
+    enabled = false,
   },
 }
