804dbd5b965fe87894e449d7b4dad9ce00dd8622
MaoX_Yu <maoxytz@163.com>
2024-09-19 20:32:53 +0800

    fix(nvim): persistence keymaps

--- lazyvim/lua/plugins/editor/persistence.lua
+++ lazyvim/lua/plugins/editor/persistence.lua
@@ -5,9 +5,11 @@ return {
     -- stylua: ignore
     keys = {
       { "<leader>qs", false },
+      { "<leader>qS", false },
       { "<leader>ql", false },
       { "<leader>qd", false },
       { "<leader>Ss", function() require("persistence").load() end,                desc = "Restore Session"            },
+      { "<leader>SS", function() require("persistence").select() end,              desc = "Select Session"             },
       { "<leader>Sl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session"       },
       { "<leader>Sd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
     },
