3c28e86675c2b9bb4fe3e5e25140e0f8f4be8d69
MaoX_Yu <maoxytz@163.com>
2024-09-26 19:07:46 +0800

    fix(nvim): select all keymap

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -11,8 +11,8 @@ end
 -- Default: <leader> is <space>
 local maps = {
   -- stylua: ignore start
-  { from = "<leader>%",  to = "ggvG$",          mode = "n",          opts = { desc = "Select All" }                 },
-  { from = "<C-a>",      to = "<esc>ggvG$",     mode = "i",          opts = { desc = "Select All" }                 },
+  { from = "<leader>%",  to = "gg0vG$",         mode = "n",          opts = { desc = "Select All" }                 },
+  { from = "<C-a>",      to = "<esc>gg0vG$",    mode = "i",          opts = { desc = "Select All" }                 },
   { from = "gh",         to = "^",              mode = { "n", "v" }, opts = { desc = "Goto Line Start" }            },
   { from = "gl",         to = "$",              mode = { "n", "v" }, opts = { desc = "Goto Line End" }              },
   { from = "Q",          to = "q",              mode = "n",          opts = { desc = "Create Macros" }              },
