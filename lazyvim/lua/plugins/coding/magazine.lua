83a79b72486c649f26971dade1870f4045eb8143
MaoX_Yu <maoxytz@163.com>
2024-10-29 23:07:21 +0800

    feat(nvim): update cmp

--- lazyvim/lua/plugins/coding/magazine.lua
+++ lazyvim/lua/plugins/coding/magazine.lua
@@ -3,4 +3,15 @@ return {
     "iguanacucumber/magazine.nvim",
     name = "nvim-cmp",
   },
+  {
+    "iguanacucumber/mag-nvim-lsp",
+    name = "cmp-nvim-lsp",
+    lazy = true,
+    opts = {},
+  },
+  {
+    "iguanacucumber/mag-buffer",
+    name = "cmp-buffer",
+    lazy = true,
+  },
 }
