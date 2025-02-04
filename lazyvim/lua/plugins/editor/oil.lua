8ed0948fa01001b542afd7cb1458244788cc9023
MaoX_Yu <maoxytz@163.com>
2025-02-04 22:25:25 +0800

    feat(nvim): enable snacks explorer

--- lazyvim/lua/plugins/editor/oil.lua
+++ lazyvim/lua/plugins/editor/oil.lua
@@ -10,26 +10,5 @@ return {
         ["<C-c>"] = false,
       },
     },
-    keys = function()
-      if vim.fn.executable("yazi") == 1 then
-        return
-      end
-      return {
-        {
-          "<leader>e",
-          function()
-            require("oil").open_float()
-          end,
-          desc = "Open Directory",
-        },
-        {
-          "<leader>E",
-          function()
-            require("oil").open_float(vim.fn.getcwd())
-          end,
-          desc = "Open Directory (cwd)",
-        },
-      }
-    end,
   },
 }
--- lazyvim/lua/plugins/editor/yazi.lua
+++ lazyvim/lua/plugins/editor/yazi.lua
@@ -3,18 +3,7 @@ return {
     "Daydreamer-riri/yazi.nvim",
     enabled = vim.fn.executable("yazi") == 1,
     event = "VeryLazy",
-    keys = {
-      {
-        "<leader>e",
-        "<cmd>Yazi<cr>",
-        desc = "Open Yazi",
-      },
-      {
-        "<leader>E",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Open Yazi (cwd)",
-      },
-    },
+    cmd = { "Yazi" },
     opts = {},
   },
 }
