c9ada0f7f2a8542c1f6adadc13d37af718ecf1ed
MaoX_Yu <maoxytz@163.com>
2025-03-16 10:36:44 +0800

    fix(nvim): plugin cond & lazy

--- lazyvim/lua/plugins/editor/avante.lua
+++ lazyvim/lua/plugins/editor/avante.lua
@@ -4,10 +4,15 @@ local build_cmd = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File
 return {
   {
     "yetone/avante.nvim",
+    cond = not vim.g.vscode,
     version = false,
     event = "VeryLazy",
     opts = {
       provider = "deepseek",
+      cursor_applying_provider = "deepseek",
+      behaviour = {
+        enable_cursor_planning_mode = true,
+      },
       vendors = {
         deepseek = {
           __inherited_from = "openai",
@@ -29,7 +34,8 @@ return {
   },
   {
     "HakonHarnes/img-clip.nvim",
-    lazy = true,
+    cond = not vim.g.vscode,
+    event = "VeryLazy",
     opts = {
       default = {
         embed_image_as_base64 = false,
